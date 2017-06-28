fd2acf9e56da3d8fa5b144a18110997c61d549b6
sainan.liu <sainan.liu@dajie-inc.com>
2017-06-28 18:36:00 +0800

    add node

---
+++ personnel/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style type="text/css">
+html, #app {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  height: 100%;
+  max-height: 100%;
+  position: relative;
+}
+</style>
---
+++ personnel/src/components/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="index">
+    <toolbar></toolbar>
+    <note-list></note-list>
+    <editor></editor>
+  </div>
+</template>
+
+<script>
+import Toolbar from './Toolbar'
+import NoteList from './NoteList'
+import Editor from './Editor'
+
+export default {
+  name: 'Index',
+  components: {
+    Toolbar,
+    NoteList,
+    Editor
+  }
+}
+</script>
+<style type="text/css">
+html, #index {
+	width: 100%;
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  height: 100%;
+  max-height: 100%;
+  position: relative;
+}
+</style>
